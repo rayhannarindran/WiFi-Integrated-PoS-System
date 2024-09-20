b744e2e06be7defb984052a5708603f3b485411b
rayhannarindran <rayhannarindran@gmail.com>
2024-09-20 23:22:10 +0700

    splitting tokenService to token generator and qr generator - 20/09/24

--- backend/services/tokenService.js
+++ backend/services/tokenService.js
@@ -1,31 +1,41 @@
 const fs = require('fs');
 var qrcode = require('qrcode'); //qrcode creator
+const crypto = require('crypto'); //crypto module
 
-function generateToken(data, callback) {
-  // parse file to json
-  const pos_data = JSON.parse(data);
-
-  // use payment no to generate token
-  const token = pos_data.id;
-
-  // generate qr code
-  qrcode.toDataURL(token, function (err, url) {
-      if (err) {
-          console.log("Error: ", err);
-          callback(err, null);
-      }
-      callback(null, url);
-    })
-
+function generateRandomString(length) {
+  return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
 }
 
-// get data 
-const data_example = fs.readFileSync('./data_example/pos_data.json');
+function generateToken(data) {
+  // Parse file to JSON
+  const pos_data = JSON.parse(data);
 
-// generates token
-generateToken(data_example, (err, url) => {
-  if (err) {
-    console.log("Error: ", err);
+  // Use payment number to generate token
+  const token = pos_data.id + '-' + pos_data.created_at + '-' + generateRandomString(10);
+  console.log("Token:", token);
+
+  return token;
+}
+
+async function generateQR(token) {
+  try {
+    // Generate QR code using await
+    const uri = await qrcode.toDataURL(token); // Await the QR code generation
+    return uri;  // Return the QR code URL
+  } catch (err) {
+    console.log("Error:", err);
+    throw err;  // Throw the error to be handled by the caller
   }
-  console.log("URL: ", url);
-});
+}
+
+// Get Token Testing
+const data_example = fs.readFileSync('./backend/data_example/pos_data.json');
+const token = generateToken(data_example);
+(async () => {
+  try {
+    const qr_uri = await generateQR(token);
+    console.log("QR URI:", qr_uri);
+  } catch (err) {
+    console.log("Error:", err);
+  }
+} )();
