1089cd00b8ec7e2092d629017ef944d65c8218e1
rayhannarindran <rayhannarindran@gmail.com>
2024-09-21 17:38:20 +0700

    Merge branch 'main' of https://github.com/rayhannarindran/WiFi-Integrated-PoS-System

--- backend/services/dbService.js
+++ backend/services/dbService.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config();
 const Joi = require('joi');
 const mongoose = require('mongoose');
@@ -187,39 +186,10 @@ async function updateTokenRecord(token, update) {
     } finally {
         if (conn) {
             await closeConnection();
-=======
-const fs = require('fs');
-const mongoose = require('mongoose');
-const Token = require('../models/Token'); // Import your model
-const generateTokenRecord = require('./tokenService').generateTokenRecord;
-
-const mongoURI = 'mongodb://localhost:27017/pos_dummy'; // MongoDB URI
-
-//Insert token record into the database
-async function insertTokenRecord(record) {
-    try {
-        // Connect to the database
-        await mongoose.connect(mongoURI);
-
-        // Create a new token document
-        const newToken = new Token(record);
-
-        // Save the document to the database
-        await newToken.save();
-        console.log('Token inserted successfully!');
-
-    } catch (error) {
-        console.error('Error inserting token:', error);
-    } finally {
-        // Close the connection only if it's the last operation
-        if (mongoose.connection.readyState === 1) {
-            mongoose.connection.close();
->>>>>>> fbd8684f2a8fe26ebadaf6160ac4fa4e521687b9
         }
     }
 }
 
-<<<<<<< HEAD
 // TESTING FUNCTION
 async function runService() {
     try {
@@ -278,19 +248,3 @@ module.exports = {
     validateTokenRecord,
     updateTokenRecord,
 };
-=======
-// Export functions
-module.exports = {
-    insertTokenRecord
-};
-
-
-// // CHANGE WITH API CALL DATA -----------------------
-// const data_file = fs.readFileSync('./data_example/pos_data.json');
-// const pos_data = JSON.parse(data_file);
-// // -------------------------------------------------
-
-// const tokenRecord = generateTokenRecord(pos_data);
-
-// insertTokenRecord(tokenRecord);
->>>>>>> fbd8684f2a8fe26ebadaf6160ac4fa4e521687b9
